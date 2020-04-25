c7db757c6d37783b3b54f987ad4eb4677035097c
DaveFres <nanyandave@gmail.com>
2020-04-25 22:34:26 +0300

    clean up

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Learning Curve', // Title for your website.
   tagline: 'A website for my own doc',
-  url: 'https://DaveFres.github.io', // Your website URL
+  url: 'https://davefres.github.io', // Your website URL
   baseUrl: '/LearningCurve/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
