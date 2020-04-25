cbf9d9c5b244cec19e2c193bda3c9d6ea231f428
DaveFres <nanyandave@gmail.com>
2020-04-25 22:02:50 +0300

    fix(siteConfig): fix url in config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,14 +23,14 @@ const users = [
 const siteConfig = {
   title: 'Learning Curve', // Title for your website.
   tagline: 'A website for my own doc',
-  url: 'https://learning-curve.github.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://DaveFres.github.io', // Your website URL
+  baseUrl: '/LearningCurve/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'Learning Curve',
+  projectName: 'LearningCurve',
   organizationName: 'DaveFres',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
@@ -73,7 +73,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Learning Curve`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
