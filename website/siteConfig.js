27bcf48ed3e3caa4de7bcbbef51af678116a5681
DaveFres <nanyandave@gmail.com>
2020-04-25 21:26:03 +0300

    fix(siteConfig): fix gh repo

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Learning Curve', // Title for your website.
   tagline: 'A website for my own doc',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  url: 'https://learning-curve.github.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
@@ -31,7 +31,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'Learning Curve',
-  organizationName: 'facebook',
+  organizationName: 'DaveFres',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
